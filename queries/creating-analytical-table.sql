0e2bb4b0b5413994494b6b3b3674040d4dc58bfc
ryzerian <157373322+ryzerian@users.noreply.github.com>
2025-06-29 00:56:04 +0700

    updated queries with some comments as explanation

--- queries/creating-analytical-table.sql
+++ queries/creating-analytical-table.sql
@@ -1,5 +1,7 @@
-create table `rakamin-kf-analytics-464103.kimia_farma.kf_tabel_analisa` as
+create table `rakamin-kf-analytics-464103.kimia_farma.kf_tabel_analisa` as -- membuat tabel baru dengan CREATE TABLE bernama tabel_analisa
 with base as (
+  /* melakukan SELECT dengan menggunakan ALIAS pada nama kolom untuk memudahkan
+  dalam membedakan kolom dengan nama yang sama pada tabel berbeda */
   select
     ft.transaction_id,
     ft.date,
@@ -13,6 +15,7 @@ with base as (
     p.product_name,
     ft.price as actual_price,
     ft.discount_percentage,
+    -- menggunakan CASE WHEN untuk melakukan pengelompokkan gross laba sesuai ketentuan
     case
       when ft.price <= 50000 then 0.10
       when ft.price <= 100000 then 0.15
@@ -21,6 +24,9 @@ with base as (
       else 0.30
     end as persentase_gross_laba,
     ft.rating as rating_transaksi
+  /* melakukan deklarasi ALIAS untuk tabel yang digunakan, yaitu FT = final_transaction;
+  KC = kantor_cabang; dan P = product; serta dilakukan JOIN untuk kolom dengan nama yang sama
+  pada tabel berbeda agar data match */  
   from
     `rakamin-kf-analytics-464103.kimia_farma.kf_final_transaction` ft
   join
@@ -30,7 +36,8 @@ with base as (
     `rakamin-kf-analytics-464103.kimia_farma.kf_product` p
     on ft.product_id = p.product_id
 )
-
+/* menggunakan CTE untuk melakukan kalkulasi terhadap nett_sales dan nett_profit yang membutuhkan
+persentase_gross_laba terdeklarasi terlebih dahulu */
 select
   *,
   actual_price * (1-discount_percentage) as nett_sales,
